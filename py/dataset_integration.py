0ff11bbd1d213d5c02237433c8cb534d0cd924d0
ChrisKoll <christian.kolland@web.de>
2025-01-02 13:27:43 +0100

    feat: Script to integrate the data

---
+++ py/dataset_integration.py
@@ -0,0 +1,52 @@
+# Standard imports
+import os
+import tempfile
+
+# Third-party imports
+import scanpy as sc
+import scvi
+import seaborn as sns
+import torch
+
+sc.set_figure_params(figsize=(6, 6), frameon=False)
+sns.set_theme()
+torch.set_float32_matmul_precision("high")
+save_dir = tempfile.TemporaryDirectory()
+
+
+def main(**kwargs):
+    """
+    Runs the scVI model on the spatial liver dataset
+
+    Args:
+        **kwargs: Keyword arguments for the program.
+    """
+    args = {"path_to_data": kwargs.get("data", None)}
+
+    if not args["path_to_data"]:
+        raise ValueError("Path to data must be provided")
+
+    # Load data
+    adata = scvi.data.read_h5ad("args.path_to_data")
+
+    scvi.model.SCVI.setup_anndata(adata, batch_key=["Slide_name"])
+    # Standard values from tutorial
+    # https://docs.scvi-tools.org/en/stable/tutorials/notebooks/scrna/harmonization.html
+    # Gene likelihood might need to change
+    model = scvi.model.SCVI(adata, n_layers=2, n_latent=30, gene_likelihood="nb")
+    model.train()
+
+    SCVI_LATENT_KEY = "X_scVI"
+    adata.obsm[SCVI_LATENT_KEY] = model.get_latent_representation()
+
+    # Cluster dataset and visualize latent space
+    sc.pp.neighbors(adata, use_rep=SCVI_LATENT_KEY)
+    sc.tl.leiden(adata)
+
+    sc.tl.umap(adata, min_dist=0.3)
+    sc.pl.umap(
+        adata,
+        color=["Slide_name", "leiden"],
+        frameon=False,
+        ncols=1,
+    )
